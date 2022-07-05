1dfb4388dc5abf0ac64abdec9be361a9fd4abc3e
Eric Bowman <ebowman@nvidia.com>
2022-07-05 09:03:01 -0600

    Added comments and deleted code for tutorial

--- exts/omni.example.ui_window/omni/example/ui_window/window.py
+++ exts/omni.example.ui_window/omni/example/ui_window/window.py
@@ -28,7 +28,6 @@ class ExampleWindow(ui.Window):
         self.frame.style = example_window_style
         # Set the function that is called to build widgets when the window is
         # visible
-        self.frame.set_build_fn(self._build_fn)
 
     def destroy(self):
         # It will destroy all the children
@@ -57,69 +56,54 @@ class ExampleWindow(ui.Window):
             ui.Image(name=image_name, width=20, height=20)
 
     def _build_calculations(self):
-        """Build the widgets of the "Calculations" group"""
-        with ui.CollapsableFrame("Calculations", name="group", build_header_fn=self._build_collapsable_header):
-            with ui.VStack(height=0, spacing=SPACING):
-                with ui.HStack():
-                    ui.Label("Precision", name="attribute_name", width=self.label_width)
-                    ui.IntSlider(name="attribute_int")
+        # Build the widgets of the "Calculations" group
 
-                with ui.HStack():
-                    ui.Label("Iterations", name="attribute_name", width=self.label_width)
-                    ui.IntSlider(name="attribute_int", min=0, max=5)
+            # A VStack is a vertical stack and aligns widgets vertically one after the other
+
+                # An HStack is a horizontal stack and aligns widgets horizontally one after the other
+
+                    # A label displays text
+
+                    # An IntSlider lets a user choos an integer by sliding a bar back and forth
+
+                # Pairing a label with a control is a common UI comb
+
+                    # The label makes the purpose of the control clear
+
+                    # You can set the min and max value on an IntSlider
+        pass
 
     def _build_parameters(self):
-        """Build the widgets of the "Parameters" group"""
-        with ui.CollapsableFrame("Parameters", name="group", build_header_fn=self._build_collapsable_header):
-            with ui.VStack(height=0, spacing=SPACING):
-                with ui.HStack():
-                    ui.Label("Value", name="attribute_name", width=self.label_width)
-                    ui.FloatSlider(name="attribute_float")
+        # Build the widgets of the "Parameters" group
 
-                with ui.HStack():
-                    ui.Label("i", name="attribute_name", width=self.label_width)
-                    ui.FloatSlider(name="attribute_float", min=-1, max=1)
+                    # A Float Slider is similar to an Int Slider
+                    # controls a 'float' which is a number with a decimal point (A Real number)
 
-                with ui.HStack():
-                    ui.Label("j", name="attribute_name", width=self.label_width)
-                    ui.FloatSlider(name="attribute_float", min=-1, max=1)
+                    # You can set the min and max of a float slider as well
 
-                with ui.HStack():
-                    ui.Label("k", name="attribute_name", width=self.label_width)
-                    ui.FloatSlider(name="attribute_float", min=-1, max=1)
+                # Setting the labels all to the same width gives the UI a nice alignment
 
-                with ui.HStack():
-                    ui.Label("Theta", name="attribute_name", width=self.label_width)
-                    ui.FloatSlider(name="attribute_float")
+                # A few more examples of float sliders
+
+        pass
 
     def _build_light_1(self):
-        """Build the widgets of the "Light 1" group"""
-        with ui.CollapsableFrame("Light 1", name="group", build_header_fn=self._build_collapsable_header):
-            with ui.VStack(height=0, spacing=SPACING):
-                with ui.HStack():
-                    ui.Label("Orientation", name="attribute_name", width=self.label_width)
-                    ui.MultiFloatDragField(0.0, 0.0, 0.0, h_spacing=SPACING, name="attribute_vector")
+        # Build the widgets of the "Light 1" group
+        
+                    # A multi float drag field lets you control a group of floats (Real numbers)
 
-                with ui.HStack():
-                    ui.Label("Intensity", name="attribute_name", width=self.label_width)
-                    ui.FloatSlider(name="attribute_float")
+                # Notice what we use the same label width in all of the collapsable frames
+                #   This ensures that the entire UI has a consistent feel
 
-                with ui.HStack():
-                    ui.Label("Color", name="attribute_name", width=self.label_width)
+                #Feel free to copy this color widget and use it in your on UIs
+
                     # The custom compound widget
-                    ColorWidget(0.25, 0.5, 0.75)
 
-                with ui.HStack():
-                    ui.Label("Shadow", name="attribute_name", width=self.label_width)
-                    ui.CheckBox(name="attribute_bool")
+                #An example of a checkbox
+        pass
 
     def _build_fn(self):
         """
         The method that is called to build all the UI once the window is
         visible.
         """
-        with ui.ScrollingFrame():
-            with ui.VStack(height=0):
-                self._build_calculations()
-                self._build_parameters()
-                self._build_light_1()
