e92334de3866ea9438feea640bcea51eedea2bec
JenNVIDIA <107507163+JenNVIDIA@users.noreply.github.com>
2022-07-11 09:04:38 -0700

    Initial commit

--- exts/omni.example.ui_gradient_window/omni/example/ui_gradient_window/style.py
+++ exts/omni.example.ui_gradient_window/omni/example/ui_gradient_window/style.py
@@ -170,42 +170,21 @@ main_window_style = {
 }
 
 def hex_to_color(hex: int) -> tuple:
-    # convert Value from int
-    red = hex & 255
-    green = (hex >> 8) & 255
-    blue = (hex >> 16) & 255
-    alpha = (hex >> 24) & 255
-    rgba_values = [red, green, blue, alpha]
-    return rgba_values
-
-def _interpolate_color(hex_min: int, hex_max: int, intep):
-    max_color = hex_to_color(hex_max)
-    min_color = hex_to_color(hex_min)
-    color = [int((max - min) * intep) + min for max, min in zip(max_color, min_color)]
-    return (color[3] << 8 * 3) + (color[2] << 8 * 2) + (color[1] << 8 * 1) + color[0]
-
-def get_gradient_color(value, max, colors):
-    step_size = len(colors) - 1
-    step = 1.0/float(step_size)
-    percentage = value / float(max)
-
-    idx = (int) (percentage / step)
-    if idx == step_size:
-        color = colors[-1]
-    else:
-        color = _interpolate_color(colors[idx], colors[idx+1], percentage)
-    return color
+    # YOUR CODE HERE
+    pass
 
 def generate_byte_data(colors):
-    data = []
-    for color in colors:
-        data += hex_to_color(color)
+    # YOUR CODE HERE
+    pass
 
-    _byte_provider = ui.ByteImageProvider()
-    _byte_provider.set_bytes_data(data, [len(colors), 1])
-    return _byte_provider
+def _interpolate_color(hex_min: int, hex_max: int, intep):
+    # YOUR CODE HERE
+    pass
+
+def get_gradient_color(value, max, colors):
+    # YOUR CODE HERE
+    pass
 
 def build_gradient_image(colors, height, style_name):
-    byte_provider = generate_byte_data(colors)
-    ui.ImageWithProvider(byte_provider,fill_policy=omni.ui.IwpFillPolicy.IWP_STRETCH, height=height, name=style_name)
-    return byte_provider
+    # YOUR CODE HERE
+    pass
